cd5afb0c0ee9ac31f5a4db7113bb57dd6c2f0d9b
Marko Linde <marko757@gmail.com>
2022-02-16 21:07:36 +0200

    Add functionality

--- hw2/webpack-skeleton/src/index.js
+++ hw2/webpack-skeleton/src/index.js
@@ -4,9 +4,6 @@
 /*https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll  */
 /*https://sebhastian.com/javascript-queryselectorall/ */
 
-
-const operators = ["+","-","/","*"];
-
 class Equation {
 
     equation = [];
@@ -26,12 +23,63 @@ class Equation {
         console.log(JSON.stringify(this.equation));
     }
 
-    getCalculation(){
 
+    convertEquationToFullNumbers = (operators) =>{
+        let index=0;
+        let number="";
+        let compressedEquation=[];
+
+        this.equation.forEach(elem =>{
+            if (this.getIndex(operators,elem) > -1 && !(elem === '-' && index === 0)){
+                compressedEquation.push(number);
+                compressedEquation.push(elem);
+                number="";
+            } else {
+                if (number === "") number = elem;
+                else number = number.concat(this.equation[index]);
+            }
+            index++;
+        });
+        if (number !== ""){
+            compressedEquation.push(number);
+        }
+
+        this.equation = compressedEquation;
+        console.log(this.equation);
+    }
+
+    getNumberOfSymbolsInEquation = (operators) =>{
+        let counter = 0;
+
+        this.equation.forEach(elem => {
+            if (this.getIndex(operators,elem) > -1) counter +=1; 
+        });
+
+        return counter;
+    }
+
+    getIndex(operators,sign){
+        return operators.findIndex((element) => element === sign);
     }
 
     getConcatEquation = () =>{ 
         return this.equation.join('');
+    }
+
+    getIndexValue = (index) => {
+        return this.equation[index];
+    }
+
+    getPreviousValue = (index) => {
+        return this.equation[index-1];
+    }
+
+    getNextValue = (index) => {
+        return this.equation[index+1];
+    }
+
+    changeEquation = (index,value) =>{
+        if (index  >= 0) this.equation.splice(index - 1,3,value);
     }
 }
 
@@ -62,8 +110,6 @@ class CalculatorUI{
         }); 
     } 
 
-
-
     action = (action) => {
         //console.log(action);
         this.calculus.actionCaller(action);
@@ -86,37 +132,102 @@ class Calculus {
 
     equation;
     equationState;
+    operators;
 
     constructor(){
         this.equation = new Equation();
         this.equationState = undefined;
+        this.operators = ['+','-','*','/'];
     }
 
     actionCaller = (action) =>{
         console.log(action);
         console.log(JSON.stringify(this.equation));
 
-        switch (action) {
-            case 'all-clear':
-                this.equation.clear(); 
-                break;
-            case '=':
-                this.equation.getCalculation();
-                break;
-            default:
-                this.equation.addElement(action);
-                this.equationState = this.equation.getConcatEquation();
-          }
+        if (!(this.findErrorAction(action))){
+            switch (action) {
+                case 'all-clear':
+                    this.equation.clear(); 
+                    break;
+                case '=':
+                    this.equation.convertEquationToFullNumbers(this.operators);
+                    this.calculate();
+                    break;
+                default:
+                    this.equation.addElement(action);
+            }
+        }
+        this.equationState = this.equation.getConcatEquation();
     }
 
     getEquationState = () =>{
         return this.equationState;
     }
 
-    getIndex(operators, sign){
-        return operators.findIndex((element) => element === sign);
+    findErrorAction = (action) =>{
+        let error = 0;
+        if (this.equation.getConcatEquation().length === 0 && 
+            this.equation.getIndex(this.operators,action) > -1 &&
+            action !== '-') error = 1;
+        else if (this.equation.getIndex(this.operators,this.equation.equation[this.equation.equation.length-1]) > -1 &&
+            action === '=') error = 1;
+        else if (this.equation.getIndex(this.operators,this.equation.equation[this.equation.equation.length-1]) > -1 &&
+            this.equation.getIndex(this.operators,action) > -1) error = 1;
+        return error;
     }
 
+    calculate = () =>{
+        while(this.equation.getNumberOfSymbolsInEquation(this.operators)){
+            this.calculateIndexValue(this.getNextCalculationIndex());
+        }
+    }
+
+    calculateIndexValue = (startIndex) =>{ 
+        let value = NaN;
+
+        switch(this.equation.getIndexValue(startIndex)) {
+            case '*':
+                value = parseFloat(this.equation.getPreviousValue(startIndex)) * 
+                        parseFloat(this.equation.getNextValue(startIndex));
+                break;
+            case '/':
+                value = parseFloat(this.equation.getPreviousValue(startIndex)) / 
+                        parseFloat(this.equation.getNextValue(startIndex));
+                break;
+            case '+':
+                value = parseFloat(this.equation.getPreviousValue(startIndex)) + 
+                        parseFloat(this.equation.getNextValue(startIndex));
+                break;
+            case '-':
+                value = parseFloat(this.equation.getPreviousValue(startIndex)) - 
+                        parseFloat(this.equation.getNextValue(startIndex));
+                break;
+            default:
+                value;
+          }  
+          console.log('Index calculation value: ' + value);
+          this.equation.changeEquation(startIndex,value.toString());
+          console.log('Equation: ' + JSON.stringify(this.equation.equation));
+    }
+
+
+    getNextCalculationIndex = () =>{
+        let startIndex = NaN;
+    
+        if (((this.getEquationIndex('*') !== -1) && (this.getEquationIndex('*') < this.getEquationIndex('/'))) || 
+            ((this.getEquationIndex('/') === -1) && (this.getEquationIndex('*') > this.getEquationIndex('/')))) 
+                startIndex = this.getEquationIndex('*');
+        else if (this.getEquationIndex('/') >= 1) startIndex = this.getEquationIndex('/');
+        else if (this.getEquationIndex('+') >= 1) startIndex = this.getEquationIndex('+');
+        else if (this.getEquationIndex('-') >= 1) startIndex = this.getEquationIndex('-');
+    
+        console.log('Starting with index: ' + startIndex);
+        return startIndex;
+    }
+
+    getEquationIndex = (value) =>{
+        return this.equation.getIndex(this.equation.equation,value);
+    }
 }
 
 
