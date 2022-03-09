ff3d0647a3c8918bec836c7fea2366b1566c6a31
Marko Linde <marko757@gmail.com>
2022-03-09 17:01:19 +0200

    add base culture support

--- ProdApp/ProdRoad/WebApp/Program.cs
+++ ProdApp/ProdRoad/WebApp/Program.cs
@@ -1,7 +1,10 @@
+using System.Globalization;
 using DAL.App.EF;
 using Domain.App.Identity;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -37,9 +40,37 @@ builder.Services
 
 builder.Services.AddControllersWithViews();
 
+//########################culture management##########################
+var supportedCultures = builder
+    .Configuration
+    .GetSection("SupportedCultures")
+    .GetChildren()
+    .Select(x => new CultureInfo(x.Value))
+    .ToArray();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    // datetime and currency support
+    options.SupportedCultures = supportedCultures;
+    // UI translated strings
+    options.SupportedUICultures = supportedCultures;
+    // if nothing is found, use this
+    options.DefaultRequestCulture =
+        new RequestCulture(builder.Configuration["DefaultCulture"], builder.Configuration["DefaultCulture"]);
+    options.SetDefaultCulture(builder.Configuration["DefaultCulture"]);
+
+    options.RequestCultureProviders = new List<IRequestCultureProvider>
+    {
+        // Order is important, its in which order they will be evaluated
+        // add support for ?culture=ru-RU
+        new QueryStringRequestCultureProvider(),
+        new CookieRequestCultureProvider()
+    };
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//###################Configure the HTTP request pipeline.#################
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
@@ -55,6 +86,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseRequestLocalization(options: app.Services.GetService<IOptions<RequestLocalizationOptions>>()?.Value!);
 
 app.UseAuthentication();
 app.UseAuthorization();
