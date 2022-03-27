ef05fb4e4f88f7dde0d5b724c9e9dccbb6dee042
Marko Linde <marko757@gmail.com>
2022-03-27 22:46:47 +0300

    add Extensions.Base

---
+++ ProdApp/ProdRoad/Extensions.Base/IdentityExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Security.Claims;
+
+namespace Extensions.Base;
+
+public static class IdentityExtensions
+{
+    public static Guid GetUserId(this ClaimsPrincipal user) => GetUserId<Guid>(user);
+    //extension method  - this in front. is added to the class definition
+    public static TKeyType GetUserId<TKeyType>(this ClaimsPrincipal user)
+    {
+        var idClaim = user.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        if (idClaim == null)
+        {
+            throw new NullReferenceException("Claim not found!");
+        }
+
+
+        var res = (TKeyType?) TypeDescriptor.
+            GetConverter(typeof(TKeyType)).
+            ConvertFromInvariantString(idClaim.Value)!;
+        return res;
+
+    }
+}
