0c53752c96f0c8f0d3606c36e9ab89ca7658dbe0
Marko Linde <marko757@gmail.com>
2022-04-04 12:35:40 +0300

    Add get nr of jokes

--- hw5/chucknorris-jokes/src/views/categories/category.ts
+++ hw5/chucknorris-jokes/src/views/categories/category.ts
@@ -1,18 +1,33 @@
-import { bindable,  Params} from "aurelia";
+import { bindable,  IHttpClient,  Params} from "aurelia";
+import { IJoke } from "../../domain/joke/IJoke";
 import { AppState } from "../../state/AppState";
 
 export class Category {
 
-    static parameters = ['bar'];
+    //static parameters = ['bar'];
+    public jokes : IJoke[] = [];
 
     @bindable category: string;
 
-    constructor(private appState: AppState){
+    constructor(private appState: AppState,
+        @IHttpClient private http: IHttpClient){
             console.log("Category constructor");  
     }
 
     load(params: Params) {
-        console.log(params);
+        console.log(params.name);
+        this.getCategoryJokesAsync(params.name,5);
     }
 
+    async getCategoryJokesAsync(name: string, amount: number): Promise<void> {
+        try {
+          for (let index = 0; index < amount; index++) {           
+            let result = await this.http.get(`https://api.chucknorris.io/jokes/random?category=${name}`);
+            let json = await result.json();
+            console.log(json);
+            } 
+        }catch (error) {
+            console.log(error);
+            }
+    }
 }
