61758ecc7b4a42100cf8d502977c5d2643d7956c
Marko Linde <marko757@gmail.com>
2022-04-28 22:03:30 +0300

    add all cors for local dev

--- ProdApp/ProdRoad/WebApp/Program.cs
+++ ProdApp/ProdRoad/WebApp/Program.cs
@@ -67,6 +67,18 @@ builder.Services.AddAutoMapper(
 
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsAllowAll",
+        policyBuilder =>
+        {
+            policyBuilder.AllowAnyOrigin();
+            policyBuilder.AllowAnyHeader();
+            policyBuilder.AllowAnyMethod();
+        });
+});
+
+
 //########################culture management##########################
 var supportedCultures = builder
     .Configuration
@@ -110,6 +122,8 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseCors("CorsAllowAll");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
