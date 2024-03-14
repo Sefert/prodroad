31d9a32c939b397fc8f016c1c4fc9b7e2397dcfb
Marko Linde <marko757@gmail.com>
2024-03-14 22:28:02 +0200

    add baserepo with no mapping

--- ProdRoad/Base.DAL.EF/BaseEntityRepository.cs
+++ ProdRoad/Base.DAL.EF/BaseEntityRepository.cs
@@ -11,10 +11,11 @@ public class BaseEntityRepository
     }
 }
 
-public class BaseEntityRepository<TKey, TDbContext, TDomainEntity>
+public class BaseEntityRepository<TKey, TDbContext, TDomainEntity, TDalEntity>
     where TKey : IEquatable<TKey>
     where TDbContext : DbContext
     where TDomainEntity : class, IDomainEntityId
+    //where TDalEntity : class, IDomainEntityId
 {
     protected readonly TDbContext RepoDbContext;
     protected readonly DbSet<TDomainEntity> RepoDbSet;
@@ -43,6 +44,50 @@ public class BaseEntityRepository<TKey, TDbContext, TDomainEntity>
         return query;
     }
     
+    //EXIST
+    public virtual bool Exists(TKey id, TKey userId = default)
+    {
+        return CreateQuery(userId).Any(e => e.Id.Equals(id));
+    }
+    public virtual async Task<bool> ExistsAsync(TKey id, TKey userId = default)
+    {
+        return await CreateQuery(userId).AnyAsync(e => e.Id.Equals(id));
+    }
+    
+    //GET-ALL
+    public virtual IEnumerable<TDomainEntity> GetAll(TKey userId = default, bool noTracking = true)
+    {
+        return CreateQuery(userId, noTracking).ToList();
+    }
+    public virtual async Task<IEnumerable<TDomainEntity>> GetAllAsync(TKey userId = default, bool noTracking = true)
+    {
+        return await CreateQuery(userId, noTracking).ToListAsync();
+    }
+    
+    //GET-FIRST
+    public TDomainEntity? FirstOrDefault(TKey id, TKey userId = default, bool noTracking = true)
+    {
+        return CreateQuery(userId, noTracking).FirstOrDefault(m => m.Id.Equals(id));
+    }
+
+    public async Task<TDomainEntity?> FirstOrDefaultAsync(TKey id, TKey userId = default, bool noTracking = true)
+    {
+        return await CreateQuery(userId, noTracking).FirstOrDefaultAsync(m => m.Id.Equals(id));
+    }
+    
+    //ADD
+    public virtual TDomainEntity Add(TDomainEntity entity)
+    {
+        return RepoDbSet.Add(entity).Entity!;
+    }
+
+    //UPDATE
+    public virtual TDomainEntity Update(TDomainEntity entity)
+    {
+        return RepoDbSet.Update(entity).Entity!;
+    }
+    
+    //DELETE
     public virtual int Remove(TKey id, TKey userId = default)
     {
         if (userId == null)
@@ -56,6 +101,28 @@ public class BaseEntityRepository<TKey, TDbContext, TDomainEntity>
             .Where(e => e.Id.Equals(id))
             .ExecuteDelete();
     }
+    public virtual async Task<int> RemoveAsync(TDomainEntity entity, TKey userId = default)
+    {
+        if (userId == null)
+        {
+            return await RepoDbSet.Where(e => e.Id.Equals(entity.Id)).ExecuteDeleteAsync();
+        }
 
+        return await CreateQuery(userId)
+            .Where(e => e.Id.Equals(entity.Id))
+            .ExecuteDeleteAsync();
+    }
+    public virtual async Task<int> RemoveAsync(TKey id, TKey userId = default)
+    {
+        if (userId == null)
+        {
+            return await RepoDbSet
+                .Where(e => e.Id.Equals(id))
+                .ExecuteDeleteAsync();
+        }
 
+        return await CreateQuery(userId)
+            .Where(e => e.Id.Equals(id))
+            .ExecuteDeleteAsync();
+    }
 }
