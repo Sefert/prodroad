0c73d18748546da9f18a3777cdba90c31bc4c4c1
Marko Linde <marko757@gmail.com>
2024-04-10 22:35:53 +0300

    add token support and token settings

--- ProdRoad/WebApp/Program.cs
+++ ProdRoad/WebApp/Program.cs
@@ -1,9 +1,12 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 using App.Contracts.DAL;
 using App.DAL.EF;
 using App.Data;
 using App.Domain.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,30 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IAppUOW, AppUOW>();
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// clear default claims
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services
+    .AddAuthentication()
+    .AddCookie(options => { options.SlidingExpiration = true; })//increase expiration when coockie is used
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false; //https must or no
+        options.SaveToken = false;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidIssuer = builder.Configuration.GetValue<string>("JWT:issuer"),
+            ValidAudience = builder.Configuration.GetValue<string>("JWT:audience"),
+            IssuerSigningKey =
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(
+                        builder.Configuration.GetValue<string>("JWT:issuer")
+                    )
+                ),
+            ClockSkew = TimeSpan.Zero, //how much can the clock differ
+        };
+    });
+
 
 /*builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();*/
