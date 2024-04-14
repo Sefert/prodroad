7ef571848310aa457485e27c8f9cc9ee9d53915f
Marko Linde <marko757@gmail.com>
2024-04-14 23:00:30 +0300

    add material and axios

---
+++ ProdRoadUI/prod-app/src/components/nav/MainNav.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+export default function MainNav() {
+    return (
+        <header>
+            <nav
+                className="navbar topnav navbar navbar-default navbar-fixed-top navbar-light user-nav">
+                <div className="d-flex">
+
+                </div>
+            </nav>
+
+
+            
+        </header>
+    );
+}
