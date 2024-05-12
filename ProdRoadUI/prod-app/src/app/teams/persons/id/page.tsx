4d8c92a09aac7e2e664479026e02591972a61e1c
Marko Linde <marko757@gmail.com>
2024-05-12 21:20:19 +0300

    add form for elem modifications

--- ProdRoadUI/prod-app/src/app/teams/persons/id/page.tsx
+++ ProdRoadUI/prod-app/src/app/teams/persons/id/page.tsx
@@ -1,21 +1,46 @@
 "use client"
-import { Box, Card, CardActionArea, CardContent, Container, Typography } from "@mui/material"
+import { Box, Button, Card, CardActionArea, CardContent, Container, FormControl, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Paper, TextField, Typography } from "@mui/material"
 import { grey } from "@mui/material/colors"
+import { styled } from '@mui/material/styles';
 
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#fff',
+    ...theme.typography.body2, 
+    color: theme.palette.primary.dark,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+  }));
 //https://stackoverflow.com/questions/49007357/how-to-make-the-whole-card-component-clickable-in-material-ui-using-react-js
 export default function TeamsId(){
     return (
-        <Box sx={{ width: 180, float: "left", borderRight:1, borderRightColor:grey[300], 
-                borderRightStyle:'solid', height:"80vh"}}>
-            <CardActionArea sx={{ width:150, height:35}}>
-            <CardContent sx={{ width:150, height:35, padding:0}}>
-                <Card sx={{ width:150, height:35, float: "left"}}>
-                    <Typography>
-                        TERE2
-                    </Typography>
-                </Card>        
-            </CardContent>
-            </CardActionArea>
-        </Box>
+        <Item sx={{ background:'#929cb3', maxWidth:600, minWidth:300, flexShrink:"inherit", mt:5}}>
+            <FormControl sx={{padding:1, width: '100%'}} variant="outlined">
+            <TextField
+                id="outlined-number"
+                label="Nimi"
+                type="string"
+                defaultValue="Hello World"
+            />
+            </FormControl>
+            <FormControl sx={{padding:1,width: '100%' }} variant="outlined">
+                <TextField
+                    id="outlined-number"
+                    label="Nimi"
+                    type="string"
+                    defaultValue="Hello World"
+                />
+            </FormControl>
+            <FormControl sx={{padding:1,width: '100%' }} variant="outlined">
+                <TextField
+                    id="outlined-number"
+                    label="Nimi"
+                    type="string"
+                    defaultValue="Hello World"
+                />
+            </FormControl>
+            <Button variant="contained" color="success" onClick={()=>null}>
+                MUUDA
+            </Button>
+        </Item>            
     )
 }
--- ProdRoadUI/prod-app/src/components/listBar/ItemCards.tsx
+++ ProdRoadUI/prod-app/src/components/listBar/ItemCards.tsx
@@ -7,6 +7,7 @@ import { LayoutRouter } from "next/dist/server/app-render/entry-base";
 import { useRouter } from "next/navigation";
 import { useLocation } from "react-router";
 import AddIcon from '@mui/icons-material/Add';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { useState } from "react";
 
 
@@ -26,29 +27,31 @@ export default function ItemCards({
       }
     return (
 
-    <Box component="main" sx={{ width: 180, float: "left",  position:"inline" , borderRight:1, borderRightColor:grey[300], 
+    <Box component="main" sx={{float: "left", display:"inline"}}>
+        <Box sx={{ width: 180, float: "left", borderRight:1, borderRightColor:grey[300], 
                     borderRightStyle:'solid', height:"100vh"}}>
-        <Stack direction="row" spacing={1} sx={{borderBottom:1, borderBottomColor:grey[300], 
-                    borderBottomStyle:'solid'}}>
-          <IconButton aria-label="add" onClick={()=>(addTeam([...teamsList,'TeamNew']))}>
-            <AddIcon />
-            <Typography>
-              ADDTEAM
-            </Typography>
-          </IconButton>
-        </Stack>
-        {teamsList.map((text:string,index:number) => (
-          <CardActionArea sx={{ width:170, height:35,m:0.5}} onClick={()=>navTo(0)}>
-              <CardContent sx={{ width:170, height:35,p:0}}>
-                  <Card sx={{ width:170, height:35, float: "left",pl:2}}>
-                      <Typography>
-                          {text}
-                      </Typography>
-                  </Card>        
-              </CardContent>
-          </CardActionArea>
-        ))}
-        <Box component="main"sx={{float: "left" , flexGrow: 1, pl: 25, my:-4}}>
+          <Stack direction="row" spacing={1} sx={{borderBottom:1, borderBottomColor:grey[300], 
+                      borderBottomStyle:'solid'}}>
+            <IconButton aria-label="add" onClick={()=>(addTeam([...teamsList,'TeamNew']))}>
+              <AddIcon />
+              <Typography>
+                ADDTEAM
+              </Typography>
+            </IconButton>
+          </Stack>
+          {teamsList.map((text:string,index:number) => (
+            <CardActionArea sx={{ width:170, height:35,m:0.5}} onClick={()=>navTo(0)}>
+                <CardContent sx={{ width:170, height:35,p:0}}>
+                    <Card sx={{ width:170, height:35, float: "left",pl:2}}>
+                        <Typography>
+                            {text}
+                        </Typography>
+                    </Card>        
+                </CardContent>
+            </CardActionArea>
+          ))}
+        </Box>
+        <Box sx={{float: "left" , flexGrow: 1, ml:5}}>
           {children}
         </Box>
     </Box>
